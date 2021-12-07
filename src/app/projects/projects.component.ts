b651f707b0180e7bd22dfffa73a4f51a516c8b86
mattriney <61891660+mattriney@users.noreply.github.com>
2021-12-06 22:41:36 -0600

    Update projects.component.ts
    Cleaup details click action for project component

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -6,51 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
-  teamTravelerIsShown : boolean=false;
-  mauticMonitoringIsShown : boolean=false;
+  toDoAppIsShown : boolean=false;
+  studentRosterIsShown : boolean=false;
   portfolioIsShown : boolean=false;
-  pokedexIsShown : boolean=false;
-  riddleIsShown: boolean=false;
   venobox:any;
   constructor() { }
   detailOnClick(project:String){
-    if(project=="teamTraveler"){
-      this.teamTravelerIsShown = !this.teamTravelerIsShown;
-      this.mauticMonitoringIsShown=false;
-      this.portfolioIsShown =false;
-      this.pokedexIsShown=false;
-      this.riddleIsShown=false;
+    if(project=="toDoApp"){
+      this.toDoAppIsShown = !this.toDoAppIsShown;
+      this.studentRosterIsShown=false;
+      this.portfolioIsShown=false;
     }
-    else if(project=="mauticMonitoring"){
-      this.mauticMonitoringIsShown = !this.mauticMonitoringIsShown;
-      this.teamTravelerIsShown=false;
-      this.portfolioIsShown =false;
-      this.pokedexIsShown=false;
-      this.riddleIsShown=false;
+    else if(project=="studentRoster"){
+      this.studentRosterIsShown = !this.studentRosterIsShown;
+      this.toDoAppIsShown=false;
+      this.portfolioIsShown=false;
     }
     else if(project=="portfolio"){
       this.portfolioIsShown = !this.portfolioIsShown;
-      this.teamTravelerIsShown=false;
-      this.mauticMonitoringIsShown =false;
-      this.pokedexIsShown=false;
-      this.riddleIsShown=false;
+      this.toDoAppIsShown=false;
+      this.studentRosterIsShown =false;
     }
-    else if(project=="pokedex"){
-      this.pokedexIsShown = !this.pokedexIsShown;
-      this.teamTravelerIsShown=false;
-      this.mauticMonitoringIsShown =false;
-      this.portfolioIsShown=false;
-      this.riddleIsShown=false;
-    }
-    else if(project=="riddleIsShown"){
-      this.riddleIsShown = !this.riddleIsShown;
-      this.teamTravelerIsShown=false;
-      this.mauticMonitoringIsShown =false;
-      this.portfolioIsShown=false;
-      this.pokedexIsShown=false;
 
-    }
-    
 
   }
   onMouse(idLink:String, idImage:String){
@@ -61,7 +38,7 @@ export class ProjectsComponent implements OnInit {
       $('#' + idImage).css( "opacity", "1" );
       $('#' +idLink).css( "opacity", "0" );
     }
-      
+
     );
 
     $( '#' + idImage ).on("mouseenter", function() {
@@ -75,13 +52,11 @@ export class ProjectsComponent implements OnInit {
   }
   ngOnInit(): void {
    this.onMouse("portfolio-link","portfolio-img");
-   this.onMouse("teamTraveler-link","teamTraveler-img");
-   this.onMouse("mautic-link","mautic-img");
-   this.onMouse("pokedex-link","pokedex-img");
-   this.onMouse("riddle-link","riddle-img");
+   this.onMouse("toDoApp-link","toDoApp-img");
+   this.onMouse("roster-link","roster-img");
    this.venobox=$('.venobox');
-   this.venobox.venobox(); 
-   
+   this.venobox.venobox();
+
   }
 
 }
