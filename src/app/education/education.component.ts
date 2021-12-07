1ab8902d34ececc0faecce04bd7eb2c8dd6590f0
mattriney <61891660+mattriney@users.noreply.github.com>
2021-12-07 00:02:39 -0600

    Cleanup education component

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -9,36 +9,23 @@ import * as awesom from '@fortawesome/free-solid-svg-icons';
 
 })
 export class EducationComponent implements OnInit {
-  stageIsShown: boolean = false ;
-  freelanceIsShown : boolean = false;
-  alternanceIsShown:boolean = false;
-  freelanceScotfyIsShown:boolean = false;
-  planeIcon: any ;
-  gameIcon:any;
-  volleyBallIcon:any;
+  expressScriptsIsShown : boolean = false;
+  codefiIsShown:boolean = false;
   detailOnClick(id:String) {
-    if(id=="alternance_detail"){
-      this.alternanceIsShown = !this.alternanceIsShown;
+    if(id=="expressScripts_detail"){
+     this.expressScriptsIsShown = !this.expressScriptsIsShown;
     }
-    else if(id=="freelance_detail"){
-     this.freelanceIsShown = !this.freelanceIsShown;
+    else if(id=="codefi_detail"){
+      this.codefiIsShown = !this.codefiIsShown;
     }
-    else if(id=="stage_detail"){
-      this.stageIsShown = !this.stageIsShown;
-    }
-    else if(id=="freelanceScotfy_detail"){
-      this.freelanceScotfyIsShown = !this.freelanceScotfyIsShown;
-    }
-   
+
   }
- 
-  constructor() { 
+
+  constructor() {
   }
 
   ngOnInit(): void {
-    this.planeIcon=awesom.faPlane;
-    this.gameIcon=awesom.faGamepad;
-    this.volleyBallIcon=awesom.faVolleyballBall;
+
   }
 
 }
